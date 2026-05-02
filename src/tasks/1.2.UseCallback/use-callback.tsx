3275af8216aec20b7def25eb8c909984b87de448
Никита Тимофеев <freemorobskyr@gmail.com>
2026-05-02 18:31:32 +0500

    отредачил решение: добавил список, сделал нормальное добавлеине элементов, добавил логи для рендеров

--- src/tasks/1.2.UseCallback/use-callback.tsx
+++ src/tasks/1.2.UseCallback/use-callback.tsx
@@ -1,49 +1,82 @@
 import { useCallback, useState, memo } from "react";
 
 export function Counter() {
-  const [count, setCount] = useState<number>(0);
+  const [countA, setCountA] = useState<number>(0);
+  const [countB, setCountB] = useState<number>(0);
+  const [countC, setCountC] = useState<number>(0);
 
-  const incrementA = () => {
-    setCount((prev) => (prev += 1));
-    console.log("клик без use-callback");
+  const incrementB = () => {
+    setCountB((prev) => prev + 1);
   };
 
-  const incrementB = useCallback(() => {
-    setCount((prev) => (prev += 1));
-    console.log("Клик с use-callback");
+  const incrementC = useCallback(() => {
+    setCountC((prev) => prev + 1);
   }, []);
 
   return (
     <>
       <div>
-        <ButtonWithoutMemo textButton={`${count}`} onIncrement={incrementA} />
+        <p>countA: {countA}</p>
+        <p>countB: {countB}</p>
+        <p>countC: {countC}</p>
       </div>
-      <div>
-        <ButtonWithMemo textButton={`${count}`} onIncrement={incrementA} />
+      <div
+        style={{
+          marginLeft: 10,
+        }}
+      >
+        <p>обычный кнопка</p>
+        <button onClick={() => setCountA((prev) => prev + 1)}>+1</button>
       </div>
-      <div>
-        <ButtonWithMemo textButton={`${count}`} onIncrement={incrementB} />
-      </div>
+      <ListWithoutMemo
+        label="без memo"
+        textButton="+1"
+        onIncrement={incrementB}
+      />
+      <ListWithMemo
+        label="c memo (без use-callback)"
+        textButton="+1"
+        onIncrement={incrementB}
+      />
+      <ListWithMemo
+        label="с memo и use-callback"
+        textButton="+1"
+        onIncrement={incrementC}
+      />
     </>
   );
 }
 
-function ButtonWithoutMemo({
-  onIncrement,
+function List({
+  label,
   textButton,
+  onIncrement,
 }: {
-  onIncrement: () => void;
+  label: string;
   textButton: string;
+  onIncrement: () => void;
 }) {
-  return <button onClick={onIncrement}>{textButton}</button>;
+  console.log("render-" + label);
+
+  const items = ["el-1", "el-2", "el-3"];
+
+  return (
+    <div
+      style={{
+        marginLeft: 10,
+      }}
+    >
+      <p>{label}</p>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item} <button onClick={onIncrement}>{textButton}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-const ButtonWithMemo = memo(function ({
-  onIncrement,
-  textButton,
-}: {
-  onIncrement: () => void;
-  textButton: string;
-}) {
-  return <button onClick={onIncrement}>{textButton}</button>;
-});
+const ListWithoutMemo = List;
+const ListWithMemo = memo(List);
